880063f8483db2f5396be8a4b3e426c9989e3cf6
JoyceKirkland <joyce84739879@163.com>
2021-01-21 20:21:17 +0800

    主机更改test

--- rm-master/Realsense/real_main.cpp
+++ rm-master/Realsense/real_main.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: joyce
  * @Date: 2021-01-21 15:17:19
- * @LastEditTime: 2021-01-21 20:09:52
+ * @LastEditTime: 2021-01-21 20:21:15
  * @LastEditors: Please set LastEditors
  * @Description:: 
  */
@@ -11,6 +11,7 @@
 //#include "realsense.h"
 
 //笔记本更改test
+//主机更改test
 int main() try
 {
     mineral min;
